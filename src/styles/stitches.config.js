c2958aa7778b900e5ae4247b0cea77c6b6446f6a
Renato Souza <natoicm@gmail.com>
2023-06-19 11:32:34 -0300

    chore: add new colors in the stitches config

--- src/styles/stitches.config.js
+++ src/styles/stitches.config.js
@@ -1,13 +1,20 @@
 import { createStitches } from '@stitches/react'
 
-export const { styled, getCssText, globalCss } = createStitches({
+export const { styled, getCssText, globalCss, theme } = createStitches({
   theme: {
     colors: {
       brandColor: '#9333EA',
       brandColorLight: '#A855F7',
+
+      statusError: '#FB7185',
       
       gray50: '#F9FAFB',
       gray100: '#F3F4F6',
+      gray200: '#E5E7EB',
+      gray300: '#D1D5DB',
+      gray400: '#9CA3AF',
+      gray600: '#4B5563',
+      gray700: '#374151',
       gray900: '#111827',
     }
   }
