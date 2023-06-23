4d8628ac76ed538756a95175e70fab8845fc0c3b
Renato Souza <natoicm@gmail.com>
2023-06-22 23:54:33 -0300

    feat: add card front componente

---
+++ src/components/Card/components/CardFront/index.tsx
@@ -0,0 +1,90 @@
+import Image from "next/image";
+import { ContactlessPayment } from '@phosphor-icons/react'
+
+import visaLogo from '@/assets/visa-logo.png';
+import { theme } from "@/styles/stitches.config";
+
+import { 
+  CVVContent, 
+  CardContainer, 
+  CardData, 
+  CardHeader, 
+  CardHoldersName, 
+  CardNumberContent, 
+  CardNumberGroup, 
+  CardNumberText, 
+  TextEmpty
+} from "./styles";
+
+interface CardFrontProps {
+  cardNumber: string;
+  holdersName: string;
+  expirationDate: string;
+}
+
+export function CardFront(props: CardFrontProps) {
+  const { colors } = theme;
+  const { expirationDate, cardNumber, holdersName } = props;
+
+  const filledCardNumber = cardNumber.padEnd(16, '*');
+  const filledExpirationDate = expirationDate.padEnd(4, '*');
+
+  function fillCardNumberMask() {
+    let groupData: JSX.Element[] = [];
+    let groupItems: JSX.Element[] = [];
+
+    for (let i = 0; i < filledCardNumber.length; i++) {
+      if (filledCardNumber[i] === '*') {
+        groupItems.push(
+          <TextEmpty />
+        )
+      } else {
+        groupItems.push(
+          <CardNumberText>
+            {filledCardNumber[i]}
+          </CardNumberText>
+        )
+      }
+      
+      if ((i + 1) % 4 === 0) {
+        groupData.push(
+          <CardNumberGroup>
+            {groupItems}
+          </CardNumberGroup>
+        )
+        groupItems = [];
+      }
+    }
+
+    return groupData;
+  }
+
+  return (
+    <CardContainer>
+      <CardHeader>
+        <Image src={visaLogo.src} alt="" width={32} height={12} />
+        <ContactlessPayment size={24} color={colors.gray50.toString()} />
+      </CardHeader>
+
+      <CardData>
+        <CardNumberContent>
+          {fillCardNumberMask()}
+        </CardNumberContent>
+
+        <div>
+          <CardHoldersName empty={!holdersName}>
+            {holdersName ?? 'Seu nome aqui'}
+          </CardHoldersName>
+          
+          <CVVContent>
+            {filledExpirationDate[0] === '*' ? <TextEmpty /> : expirationDate[0]}
+            {filledExpirationDate[1] === '*' ? <TextEmpty /> : expirationDate[1]}
+            /
+            {filledExpirationDate[2] === '*' ? <TextEmpty /> : expirationDate[2]}
+            {filledExpirationDate[3] === '*' ? <TextEmpty /> : expirationDate[3]}
+          </CVVContent>
+        </div>
+      </CardData>
+    </CardContainer>
+  )
+}
---
+++ src/components/Card/components/CardFront/styles.ts
@@ -0,0 +1,92 @@
+import { styled } from "@/styles/stitches.config";
+
+import bgImage from '@/assets/card-bg.png'
+
+export const CardContainer = styled('div', {
+  width: '28rem',
+  height: '16.8rem',
+  borderRadius: 16,
+  border: '1px solid $gray700',
+  padding: '2.4rem',
+  backgroundColor: '$gray900',
+  boxShadow: '0px 4px 24px rgba(0, 0, 0, 0.25)',
+  backgroundImage: `url(${bgImage.src})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+
+  cursor: 'pointer',
+
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between'
+})
+
+export const CardHeader = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between'
+})
+
+export const CardData = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 12,
+
+  '& > div': {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
+})
+
+export const CardHoldersName = styled('span', {
+  fontSize: '1.4rem',
+  fontWeight: '400',
+  color: '$gray50',
+
+  variants: {
+    empty: {
+      true: {
+        opacity: 0.5,
+      }
+    }
+  }
+})
+
+export const CardNumberContent = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between'
+})
+
+export const CardNumberText = styled('strong', {
+  fontSize: '1.6rem',
+  fontWeight: '600',
+  color: '$gray50',
+})
+
+export const CardNumberGroup = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: 4,
+})
+
+export const TextEmpty = styled('div', {
+  width: 4,
+  height: 4,
+  borderRadius: '100%',
+  backgroundColor: '$gray50',
+  margin: '0 1px',
+  opacity: .5
+})
+
+export const CVVContent = styled('span', {
+  display: 'flex',
+  alignItems: 'center',
+  gap: 4,
+
+  fontSize: '1.4rem',
+  fontWeight: '400',
+  color: '$gray50',
+  letterSpacing: 1.4
+})
